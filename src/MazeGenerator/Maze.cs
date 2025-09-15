39dbc909cf13aa5cc997c62a8e8ae9c24afbf80a
jensdeg <jensdegraaf88@gmail.com>
2025-09-15 17:32:31 +0200

    cleanup

--- src/MazeGenerator/Maze.cs
+++ src/MazeGenerator/Maze.cs
@@ -6,6 +6,26 @@
 
         public byte[,] Grid { get; private set; } = new byte[size, size];
 
+        public Maze GenerateWalls()
+        {   
+            if(Size % 2 == 0) // can't generate walls on even sized grid
+            {
+                Size++;
+                Grid = new byte[Size, Size];
+            }
+            for (int x = 0; x < Size; x++)
+            {
+                for (int y = 0; y < Size; y++)
+                {
+                    if (x % 2 == 0 || y % 2 == 0)
+                    {
+                        Grid[x, y] = 1;
+                    }
+                }
+            }
+            return this;
+        }
+
         public override string ToString()
         {
             var gridString = string.Empty;
--- src/MazeGenerator/MazeGen.cs
+++ src/MazeGenerator/MazeGen.cs
@@ -15,29 +15,8 @@
 
         private static Maze Kruskals(uint size)
         {
-            Maze maze = new(size);
-
-            // Generate walls
-            for (int x = 0; x < size; x++) 
-            {
-                if(x %  2 == 0)
-                {
-                    for (int y = 0; y < size; y++)
-                    {
-                        maze.Grid[x, y] = 1;
-                    }
-                }
-            }
-            for (int y = 0; y < size; y++)
-            {
-                if (y % 2 == 0)
-                {
-                    for (int x = 0; x < size; x++)
-                    {
-                        maze.Grid[x, y] = 1;
-                    }
-                }
-            }
+            var maze = new Maze(size)
+                .GenerateWalls();
 
             return maze;
         }
