7f445e9bf31223fe4a65f8c18642522a0e2d25ad
jensdeg <jensdegraaf88@gmail.com>
2025-09-25 21:07:24 +0200

    tiny bit more efficient maze solver

--- src/MazeGenerator/MazeSolver.cs
+++ src/MazeGenerator/MazeSolver.cs
@@ -14,13 +14,18 @@
         private static void SetNeighbours(
             this IReadOnlyCollection<Point> neighbours, Maze maze)
         {
+            var size = maze.Grid.GetLength(0) - 1;
+
             foreach (var p in neighbours) 
             {
-                var size = maze.Grid.GetLength(0) - 1;
-                if(p.X < 0 || p.Y < 0 || p.X > size || p.Y > size) continue;
-                if (maze.Grid[p.X, p.Y] >= 1) continue;
+                if (maze.Grid[maze.End.X, maze.End.Y] == 2) return;
+
+                if(p.X < 0 || p.Y < 0 || p.X > size || p.Y > size ||
+                    maze.Grid[p.X, p.Y] >= 1) continue;
+
                 maze.SetPoint(p, 2);
-                if(maze.PointCount(0) > 0 && maze.Grid[maze.End.X, maze.End.Y] != 2)
+
+                if(maze.PointCount(0) > 0)
                 {
                     p.Neighbours.SetNeighbours(maze);
                 }
